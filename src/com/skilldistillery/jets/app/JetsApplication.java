6951a4f6bb4c1e3d3e0c0dbeb9378c372a91d146
William Brown <wsbrown.colo@gmail.com>
2024-11-08 14:43:55 -0700

    Stub application and AirField classes

--- src/com/skilldistillery/jets/app/JetsApplication.java
+++ src/com/skilldistillery/jets/app/JetsApplication.java
@@ -1,10 +1,31 @@
 package com.skilldistillery.jets.app;
 
+import java.util.Scanner;
+
 public class JetsApplication {
 
+	private AirField airField = new AirField();
+	private static Scanner kb;
+	
+	// NO other fields! No list of jets!
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		kb = new Scanner(System.in);
+		JetsApplication app = new JetsApplication();
+		app.launch();
+		
+		
+		
+		
+		
+		kb.close();
 	}
 
+	private void launch() {
+		// TODO Auto-generated method stub
+		}
+
+	private void displayUserMenu() {
+		//TODO sysouts for menu
+	}
 }
