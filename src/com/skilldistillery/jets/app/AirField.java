98a9f74822ce3e711d742346ef0d35820c32bbcf
William Brown <wsbrown.colo@gmail.com>
2024-11-08 17:21:48 -0700

    user story 3 complete

--- src/com/skilldistillery/jets/app/AirField.java
+++ src/com/skilldistillery/jets/app/AirField.java
@@ -2,53 +2,60 @@ package com.skilldistillery.jets.app;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.skilldistillery.jets.entities.CargoJet;
+import com.skilldistillery.jets.entities.FighterJet;
 import com.skilldistillery.jets.entities.Jet;
 
 public class AirField {
-	
-	String fileName = "Jets.Text";
-	
-	private List<Jet> fleet = new ArrayList<>();
+
+	List<Jet> fleet = new ArrayList<>();
 	// NO other fields
-	
+
 	public AirField() {
 		loadJetsFromFile("jets.txt");
+	//	System.out.println(fleet);
 	}
-	
+
 	private void loadJetsFromFile(String fileNameJets) {
-		//FIXME - try/catch/bufferedreader template
-		//split line form file into array
-		//Jet newJet = new PassengerJet(mod,sp,rng,pr);
-		//fleet,add(newJet)
-		
-		try {
-			BufferedReader br = new BufferedReader(new FileReader(fileName)) {
-				String line = null;
-				while ((line = br.readLine()) !=null) {
-					String[] fields = line.split(",");
-					String jetType = fields[0];
-					String model = fields[1];
-					int speed = Integer.parseInt(fields[2]);
-					int range = Integer.parseInt(fields[3]);
-					double price = Double.parseDouble(fields[4]);
-					//use jetType to determine which subclass of Jet to construct
-					switch(jetType) {
-					case "cargo":
-						// construct a new CargoJet()
-						// add to fleet
-						break;
-					}
-					//Jet newJet = new PassengerJet(mod,sp,rng,pr);
-					// fleet.add(newJet)
+		try (BufferedReader br = new BufferedReader(new FileReader(fileNameJets))) {
+			String line = null;
+			while ((line = br.readLine()) != null) {
+				String[] fields = line.split(",");
+				String jetType = fields[0];
+				String model = fields[1];
+				int speed = Integer.parseInt(fields[2]);
+				int range = Integer.parseInt(fields[3]);
+				double price = Double.parseDouble(fields[4]);
+
+				Jet newJet = null;
+
+				switch (jetType) {
+				case "Cargo":
+					newJet = new CargoJet(model, speed, range, price);
+					fleet.add(newJet);
+					break;
+
+				case "fighter":
+					newJet = new FighterJet(model, speed, range, price);
+					fleet.add(newJet);
+					break;
+
+				case "passenger":
+					newJet = new FighterJet(model, speed, range, price);
+					fleet.add(newJet);
+					break;
 				}
+				System.out.println(newJet);
+				
 			}
-			catch (fileNotFoundException e)
+		} catch (IOException e) {
+			System.err.println(e);
 		}
+
 	}
-	
-	//TODO: public methods for handling jets
 
 }
