8b9a6d5c39f34b3fee6b82450fc3ee2f5879caa5
William Brown <wsbrown.colo@gmail.com>
2024-11-09 11:19:17 -0700

    established CargoCarrier and CombatReady - complete

---
+++ src/com/skilldistillery/jets/entities/CargoCarrier.java
@@ -0,0 +1,7 @@
+package com.skilldistillery.jets.entities;
+
+public abstract class CargoCarrier {
+
+	public abstract void loadCargo();
+	
+}
---
+++ src/com/skilldistillery/jets/entities/CombatReady.java
@@ -0,0 +1,7 @@
+package com.skilldistillery.jets.entities;
+
+public abstract class CombatReady {
+	
+	public abstract void combatReady();
+
+}
